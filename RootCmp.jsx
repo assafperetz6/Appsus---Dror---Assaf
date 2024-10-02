1d2e34dc526227fa97dbdaf590f6763cc8903837
assafperetz6 <assafperetz6@gmail.com>
2024-10-02 11:59:54 +0300

    Add user msg cmp

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -6,7 +6,7 @@ import { About } from "./pages/About.jsx"
 import { Home } from "./pages/Home.jsx"
 import { MailIndex } from "./apps/mail/pages/MailIndex.jsx"
 import { NoteIndex } from "./apps/note/pages/NoteIndex.jsx"
-
+import { UserMsg } from "./cmps/UserMsg.jsx"
 
 
 export function App() {
@@ -19,6 +19,8 @@ export function App() {
                 <Route path="/mail" element={<MailIndex />} />
                 <Route path="/note" element={<NoteIndex />} />
             </Routes>
+
+            <UserMsg />
         </section>
     </Router>
 }

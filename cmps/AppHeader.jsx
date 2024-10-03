e1489f983f84009416251ca303b2330c2cac65fd
assafperetz6 <assafperetz6@gmail.com>
2024-10-03 19:24:52 +0300

    Refactor all icons to pseudo elements

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -16,7 +16,7 @@ export function AppHeader() {
 		<header className="app-header full">
 			<section className="logo-menu-container flex">
 				<button className="main-menu-btn">
-					<svg width="24" height="24" viewBox="0 0 24 24">
+					<svg width="24" height="24" fill="#616367" viewBox="0 0 24 24">
 						<path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
 					</svg>
 				</button>
--- cmps/MainMenu.jsx
+++ cmps/MainMenu.jsx
@@ -7,79 +7,54 @@ export function MainMenu() {
 		<ul className="main-menu clean-list">
 			<li>
 				<button className="compose">
-					<span className="material-symbols-outlined">edit</span>
-					<div>Compose</div>
+					Compose
 				</button>
 			</li>
 			<li>
-				<button onClick={() => setSearchPrms({status: 'inbox'})}>
-					<div className="flex space-between align-center">
-                        <span className="material-symbols-outlined">inbox</span>
-                        <span>Inbox</span>
-                    </div>
+				<button className="inbox" onClick={() => setSearchPrms({status: 'inbox'})}>
+					Inbox
                     <span className="mail-counter">23</span>
 				</button>
 			</li>
 			<li>
-				<button onClick={() => setSearchPrms({status: 'starred'})}>
-					<div className="flex space-between align-center">
-                        <span className="material-symbols-outlined">star</span>
-                        <span>Starred</span>
-                    </div>
+				<button className="starred" onClick={() => setSearchPrms({status: 'starred'})}>
+					Starred
                     <span className="mail-counter">23</span>
 				</button>
 			</li>
 			<li>
-				<button onClick={() => setSearchPrms({status: 'snoozed'})}>
-					<div className="flex space-between align-center">
-                        <span className="material-symbols-outlined">schedule</span>
-                        <span>Snoozed</span>
-                    </div>
+				<button className="snoozed" onClick={() => setSearchPrms({status: 'snoozed'})}>
+					Snoozed
                     <span className="mail-counter">23</span>
 				</button>
 			</li>
 			<li>
-				<button onClick={() => setSearchPrms({status: 'important'})}>
-					<div className="flex space-between align-center">
-                        <span className="material-symbols-outlined">label_important</span>
-                        <span>Important</span>
-                    </div>
+				<button className="important" onClick={() => setSearchPrms({status: 'important'})}>
+					Important
                     <span className="mail-counter">23</span>
 				</button>
 			</li>
 			<li>
-				<button onClick={() => setSearchPrms({status: 'sent'})}>
-					<div className="flex space-between align-center">
-                        <span className="material-symbols-outlined">send</span>
-                        <span>Sent</span>
-                    </div>
+				<button className="sent" onClick={() => setSearchPrms({status: 'sent'})}>
+					Sent
                     <span className="mail-counter">23</span>
 				</button>
 			</li>
 			<li>
-				<button onClick={() => setSearchPrms({status: 'drafts'})}>
-					<div className="flex space-between align-center">
-                        <span className="material-symbols-outlined">draft</span>
-                        <span>Drafts</span>
-                    </div>
+				<button className="drafts" onClick={() => setSearchPrms({status: 'drafts'})}>
+					Drafts
                     <span className="mail-counter">23</span>
 				</button>
 			</li>
 			<li>
-				<button>
-					<div className="flex space-between align-center">
-                        <span className="material-symbols-outlined">label</span>
-                        <span>Categories</span>
-                    </div>
+				<button className="categories">
+					Categories
                     <span>23</span>
 				</button>
 			</li>
 			<li>
-				<button>
-					<div className="flex space-between align-center">
-                        <span className="material-symbols-outlined">chevron_right</span>
-                        <span>More</span>
-                    </div>
+				<button className="see-more">
+					More
                     <span>23</span>
 				</button>
 			</li>

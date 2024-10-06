0bebb85afe067223c106b76c139afaf452f1fb0f
DrorGaon <drordtuii@gmail.com>
2024-10-06 17:04:22 +0300

    Simplify dataset for development stage

--- apps/note/services/dataset.js
+++ apps/note/services/dataset.js
@@ -16,34 +16,34 @@ export const defaultNotes = [
             fontSize: '16px'
         },
     },
-    {
-        id: utilService.makeId(),
-        createdAt: Date.now(),
-        type: 'NoteTodo',
-        isPinned: false,
-        title: 'This is a todo list note',
-        info: {
-            todos:[
-                {txt: 'This has been done already', doneAt: Date.now()},
-                {txt: 'This hasn\'t', doneAt: null}
-            ]
-        },
-        style: {
-            backgroundColor: '#000',
-            fontSize: '16px'
-        },
-    },
-    {
-        id: utilService.makeId(),
-        createdAt: Date.now(),
-        type: 'NoteImg',
-        isPinned: false,
-        title: 'This is an image note',
-        info: {
-            url: 'https://www.ucpress.edu/_next/image?url=https%3A%2F%2Fwebfiles.ucpress.edu%2Fcoverimage%2Fisbn13%2F9780520236943.jpg&w=640&q=90'
-        },
-        style: {
-            backgroundColor: '#00d'
-        },
-    },
+    // {
+    //     id: utilService.makeId(),
+    //     createdAt: Date.now(),
+    //     type: 'NoteTodo',
+    //     isPinned: false,
+    //     title: 'This is a todo list note',
+    //     info: {
+    //         todos:[
+    //             {txt: 'This has been done already', doneAt: Date.now()},
+    //             {txt: 'This hasn\'t', doneAt: null}
+    //         ]
+    //     },
+    //     style: {
+    //         backgroundColor: '#000',
+    //         fontSize: '16px'
+    //     },
+    // },
+    // {
+    //     id: utilService.makeId(),
+    //     createdAt: Date.now(),
+    //     type: 'NoteImg',
+    //     isPinned: false,
+    //     title: 'This is an image note',
+    //     info: {
+    //         url: 'https://www.ucpress.edu/_next/image?url=https%3A%2F%2Fwebfiles.ucpress.edu%2Fcoverimage%2Fisbn13%2F9780520236943.jpg&w=640&q=90'
+    //     },
+    //     style: {
+    //         backgroundColor: '#00d'
+    //     },
+    // },
 ]

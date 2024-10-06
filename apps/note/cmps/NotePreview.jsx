2824ca0be6f76d9fcdd616980604cd70cc42ac99
DrorGaon <drordtuii@gmail.com>
2024-10-06 17:36:00 +0300

    Improve note rendering

--- apps/note/cmps/NotePreview.jsx
+++ apps/note/cmps/NotePreview.jsx
@@ -2,9 +2,12 @@
 
 export function NotePreview({ note }){
 
+    const { style, info } = note
+
     return (
-        <li>
-            {note.title}
+        <li style={style} className="note-preview">
+            <h2 className="note-title">{note.title}</h2>
+            <p>{info.txt}</p>
         </li>
     )
 }

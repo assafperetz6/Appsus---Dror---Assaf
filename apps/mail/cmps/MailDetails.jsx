2e7a1b24c3c8ed7701ac78bfc5b6123ff2bbf124
assafperetz6 <assafperetz6@gmail.com>
2024-10-12 14:12:18 +0300

    Halfway design for mailDetails

--- apps/mail/cmps/MailDetails.jsx
+++ apps/mail/cmps/MailDetails.jsx
@@ -1,3 +1,56 @@
 export function MailDetails() {
-    return <div>Heloo!!!</div>
+    return (
+        <article className="details-container">
+            <section className="actions-pagintation">
+				<section className="details-actions">
+					<button className="go-back"></button>
+
+					<div className="flex space-evenly">
+                        <button className='archive'></button>
+                        <button className='report'></button>
+                        <button className='trash'></button>
+                    </div>
+					<div className="flex space-evenly">
+                        <button className='mail-unread'></button>
+                        <button className='move-to'></button>
+                    </div>
+				</section>
+
+				<section className="info-pagination flex">
+					<div className="shown-mails">1-50 of 2,000</div>
+					<button>
+						<span className="material-symbols-outlined">chevron_left</span>
+					</button>
+					<button>
+						<span className="material-symbols-outlined">chevron_right</span>
+					</button>
+				</section>
+            </section>
+
+            <section className="details-header">
+                <h2 className="subject">PlaceHolder</h2>
+                <button className="important"></button>
+            </section>
+
+            <section className="mail-info">
+                <div className="img-container"><img className="profile-pic" src="../../assets/img/unknown-profile.png" alt="" /></div>
+                <table>
+                    <tbody className="mail-body">
+                        <tr>
+                            <td><h3>placeholder@gmail.com</h3></td>
+                            <td>Oct 9, 2024, 4:52PM (3 days ago)</td>
+                            <td><button className="starred"></button></td>
+                            <td className="reply"></td>
+                        </tr>
+                        <tr><td><h6>to me</h6></td></tr>
+                        <tr><td>Actual Mail body</td></tr>
+                    </tbody>
+                </table>
+                <section className="reply-actions">
+                    <button className="reply">Reply</button>
+                    <button className="forward">Forward</button>
+                </section>
+            </section>
+        </article>
+    )
 }

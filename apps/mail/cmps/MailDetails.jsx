bd6b88b7a57d4ef74b382246a9589661b5a1fba3
assafperetz6 <assafperetz6@gmail.com>
2024-10-12 19:21:22 +0300

    mailDetails renders actual data

--- apps/mail/cmps/MailDetails.jsx
+++ apps/mail/cmps/MailDetails.jsx
@@ -1,21 +1,59 @@
-// import { mailService } from "../services/mail.service"
+const { useState, useEffect } = React
+const { useParams } = ReactRouterDOM
+
+import { mailService } from '../services/mail.service.js'
+import { Loader } from '../../../cmps/Loader.jsx'
 
 export function MailDetails() {
-    return (
-        <article className="details-container">
-            <section className="actions-pagintation">
+	const [mail, setMail] = useState(null)
+	const { mailId } = useParams()
+    const { mail: loggedinUser } = mailService.loggedinUser
+
+	useEffect(() => {
+		if (mailId) loadMail()
+	}, [])
+
+	function loadMail() {
+		mailService
+			.get(mailId)
+			.then(setMail)
+			.catch((err) => {
+				console.log('Problem getting mail', err)
+				navigate('/mail')
+			})
+	}
+
+    function formatTimestamp(timestamp) {
+        const options = { 
+            year: 'numeric', 
+            month: 'short', 
+            day: 'numeric', 
+            hour: 'numeric', 
+            minute: 'numeric', 
+            hour12: true 
+        }
+        return new Date(timestamp).toLocaleString('en-US', options)
+    }
+    
+
+    if (!mail) return <Loader/>
+
+	const { subject, body, isStarred, isImportant, sentAt, from, to } = mail
+	return (
+		<article className="details-container">
+			<section className="actions-pagintation">
 				<section className="details-actions">
 					<button className="go-back"></button>
 
 					<div className="flex space-evenly">
-                        <button className='archive'></button>
-                        <button className='report'></button>
-                        <button className='trash'></button>
-                    </div>
+						<button className="archive"></button>
+						<button className="report"></button>
+						<button className="trash"></button>
+					</div>
 					<div className="flex space-evenly">
-                        <button className='mail-unread'></button>
-                        <button className='move-to'></button>
-                    </div>
+						<button className="mail-unread"></button>
+						<button className="move-to"></button>
+					</div>
 				</section>
 
 				<section className="info-pagination flex">
@@ -27,32 +65,56 @@ export function MailDetails() {
 						<span className="material-symbols-outlined">chevron_right</span>
 					</button>
 				</section>
-            </section>
+			</section>
 
-            <section className="details-header">
-                <h2 className="subject">PlaceHolder</h2>
-                <button className="important"></button>
-            </section>
+			<section className="details-header">
+				<h2 className="subject">{subject}</h2>
+				<button className="important"></button>
+			</section>
 
-            <section className="mail-info">
-                <div className="img-container"><img className="profile-pic" src="../../assets/img/unknown-profile.png" alt="" /></div>
-                <table className="flex">
-                    <tbody className="mail-content">
-                        <tr>
-                            <td className="sender-address"><h3>placeholder@gmail.com</h3></td>
-                            <td className="sent-at"><span>Oct 9, 2024, 4:52PM (3 days ago)</span></td>
-                            <td><button className="starred"></button></td>
-                            <td><button className="reply"></button></td>
-                        </tr>
-                        <tr><td><h6>to me</h6></td></tr>
-                        <tr className="mail-body"><td>Actual Mail body</td></tr>
-                        <tr className="reply-actions">
-                            <td><button className="reply">Reply</button></td>
-                            <td><button className="forward">Forward</button></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </section>
-        </article>
-    )
+			<section className="mail-info">
+				<div className="img-container">
+					<img
+						className="profile-pic"
+						src="../../assets/img/unknown-profile.png"
+						alt=""
+					/>
+				</div>
+				<table className="flex">
+					<tbody className="mail-content">
+						<tr>
+							<td className="sender-address">
+								<h3>{from}</h3>
+							</td>
+							<td className="sent-at">
+								<div>{formatTimestamp(sentAt)}</div>
+							</td>
+							<td>
+								<button className="starred"></button>
+							</td>
+							<td>
+								<button className="reply"></button>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<h6>to {to === loggedinUser ? 'me' : to}</h6>
+							</td>
+						</tr>
+						<tr className="mail-body">
+							<td>{body}</td>
+						</tr>
+						<tr className="reply-actions">
+							<td>
+								<button className="reply">Reply</button>
+							</td>
+							<td>
+								<button className="forward">Forward</button>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</section>
+		</article>
+	)
 }

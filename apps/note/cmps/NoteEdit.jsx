243a3b7183f96769882ea2ea404092a6d804645a
DrorGaon <drordtuii@gmail.com>
2024-10-13 22:42:47 +0300

    Add different kinds of note creation

--- apps/note/cmps/NoteEdit.jsx
+++ apps/note/cmps/NoteEdit.jsx
@@ -7,6 +7,12 @@ const { useParams } = ReactRouterDOM
 export function NoteEdit({ saveNote }){
 
     const [note, setNote] = useState(noteService.getEmptyNote)
+    const [selectedType, setSelectedType] = useState({
+        type: 'NoteTxt',
+        placeholder: 'Take a note...',
+        infoKey: 'txt',
+    })
+
     const [isOpen, setIsopen] = useState(false)
     const { noteId } = useParams()
     const textAreaRef = useRef()
@@ -15,6 +21,10 @@ export function NoteEdit({ saveNote }){
     useEffect(() => {
         if(noteId){
             noteService.get(noteId)
+                .then((note) => {
+                    onSetType(note.type)
+                    return note
+                })
                 .then(setNote)
                 .then(() => setIsopen(true))
         } 
@@ -32,9 +42,15 @@ export function NoteEdit({ saveNote }){
     
     function onSaveNote(ev){
         ev.preventDefault()
-        if(!note.info.txt && !note.title) {
+        if(!note.info[selectedType.infoKey] && !note.title) {
             showErrorMsg('Cant save empty note')
             return
+        }
+        note.type = selectedType.type
+        if(note.type === 'NoteTodos'){
+            const todos = note.info.todos.split(',')
+            todos.forEach((todo, idx) => (todos[idx] = {txt: todo, doneAt: null} ))
+            note.info.todos = todos
         }
         setIsopen(false)
         saveNote(note)
@@ -68,6 +84,21 @@ export function NoteEdit({ saveNote }){
         setNote(prevNote => ({ ...prevNote, info }))
     }
 
+    function onSetType(type){
+        switch (type) {
+            case 'NoteTxt':
+                setSelectedType({type, placeholder: 'Take a note...', infoKey: 'txt'})
+                break    
+            case 'NoteImg':
+                setSelectedType({type, placeholder: 'Enter image url', infoKey:'url'})
+                break   
+            case 'NoteTodos':
+                setSelectedType({type, placeholder: 'Enter a comma seperated list', infoKey:'todos'})
+                break   
+        }
+        setIsopen(true)
+    }
+
     function resizeTextArea({ target }){
         target.style.height = "1px";
         target.style.height = (target.scrollHeight)+"px"
@@ -79,8 +110,10 @@ export function NoteEdit({ saveNote }){
     }
         
     const  { title, info } = note
+    const isTypeChange = !isOpen && !note.title ? true : false
     return (
-        <form onSubmit={onSaveNote} className="note-edit" onFocus={() => setIsopen(true)} onBlur={onInputBlur}>
+        <form onSubmit={onSaveNote} className="note-edit" >
+            <section className="input-container">
             <input
                 ref={inputRef}
                 value={title}
@@ -91,15 +124,25 @@ export function NoteEdit({ saveNote }){
                 placeholder="Title"
                 onChange={handleChange}
                 autoComplete="off"
-             />
+                onFocus={() => setIsopen(true)} 
+                onBlur={onInputBlur}
+                />
+            {isTypeChange && 
+                <section className="noteTypes">
+                    <button type="button" onClick={() => onSetType('Notetxt')} className="text" title="Text" ></button>
+                    <button type="button" onClick={() => onSetType('NoteImg')} className="image" title="Image" ></button>
+                    <button type="button" onClick={() => onSetType('NoteTodos')} className="todos" title="To do list" ></button>
+                </section>
+            }
+            </section>
             { isOpen && <textarea className="edit-input-text" 
                 ref={textAreaRef}
                 onChange={handleChange}
                 value={info.text}
-                name="info.txt" 
+                name={`info.${selectedType.infoKey}`} 
                 id="txt" 
                 onKeyUp={resizeTextArea}
-                placeholder="Take a note...">
+                placeholder={selectedType.placeholder}>
             </textarea>}
             <button>Save</button>
         </form>

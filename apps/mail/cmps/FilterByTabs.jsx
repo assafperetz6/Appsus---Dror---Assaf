4f2d5ac3e9b7d929dc7fe85e3e23b0b7c2667127
assafperetz6 <assafperetz6@gmail.com>
2024-10-14 14:28:12 +0300

    Change icons from span to before elements

--- apps/mail/cmps/FilterByTabs.jsx
+++ apps/mail/cmps/FilterByTabs.jsx
@@ -1,20 +1,16 @@
 export function FilterByTabs() {
 	return (
 		<section className="filter-tabs">
-			<button>
-				<span className="material-symbols-outlined">inbox</span>
+			<button className="primary">
 				Primary
 			</button>
-			<button>
-				<span className="material-symbols-outlined">sell</span>
+			<button className="promotions">
 				Promotions
 			</button>
-			<button>
-				<span className="material-symbols-outlined">group</span>
+			<button className="social">
 				Social
 			</button>
-			<button>
-				<span className="material-symbols-outlined">info</span>
+			<button className="updates">
 				Updates
 			</button>
 		</section>
